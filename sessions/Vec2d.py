e42910c43f1daf9528a97d75df8285bac74c4d69
Robert Carrillo <robertcl795@gmail.com>
2023-02-03 19:23:11 -0600

    feat: add mean squared displacement to sesion-4, add trajectories/metrics csv

---
+++ sessions/Vec2d.py
@@ -0,0 +1,46 @@
+################# http://www.pygame.org/wiki/2DVectorClass ##################
+import math
+
+class Vec2d(object):
+    """2d vector class, supports vector and scalar operators,
+       and also provides a bunch of high level functions
+       """
+    __slots__ = ['x', 'y']
+
+    def __init__(self, x_or_pair, y = None):
+        if y == None:            
+            self.x = x_or_pair[0]
+            self.y = x_or_pair[1]
+        else:
+            self.x = x_or_pair
+            self.y = y
+            
+    # Addition
+    def __add__(self, other):
+        if isinstance(other, Vec2d):
+            return Vec2d(self.x + other.x, self.y + other.y)
+        elif hasattr(other, "__getitem__"):
+            return Vec2d(self.x + other[0], self.y + other[1])
+        else:
+            return Vec2d(self.x + other, self.y + other)
+
+    # Subtraction
+    def __sub__(self, other):
+        if isinstance(other, Vec2d):
+            return Vec2d(self.x - other.x, self.y - other.y)
+        elif (hasattr(other, "__getitem__")):
+            return Vec2d(self.x - other[0], self.y - other[1])
+        else:
+            return Vec2d(self.x - other, self.y - other)
+    
+    # Vector length
+    def get_length(self):
+        return math.sqrt(self.x**2 + self.y**2)
+    
+    # rotate vector
+    def rotated(self, angle):        
+        cos = math.cos(angle)
+        sin = math.sin(angle)
+        x = self.x*cos - self.y*sin
+        y = self.x*sin + self.y*cos
+        return Vec2d(x, y)
